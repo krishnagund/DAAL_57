e88ce2f3aa58b136bd26eb7608c7939d1c96b01a
krishnagund <131687698+krishnagund@users.noreply.github.com>
2023-10-17 16:38:34 +0530

    test

--- ass_c_1.cpp
+++ ass_c_1.cpp
@@ -1,35 +1,37 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>  // Include the cstring header for memset
+
 using namespace std;
 
-// Returns the count of ways we can
-// sum coins[0...n-1] coins to get sum "sum"
-int count(int coins[], int n, int sum)
-{
-	if (sum == 0)
-		return 1;
-	if (sum < 0)
-		return 0;
+int countCoinChangeWays(int coins[], int n, int sum) {
+    // Create an array to store the number of ways for each sum
+    int dp[sum + 1];
+    memset(dp, 0, sizeof(dp));
 
-	if (n <= 0)
-		return 0;
-	return count(coins, n, sum - coins[n - 1])
-		+ count(coins, n - 1, sum);
-}
-int main()
-{
-	int i, j;
-	int coins[100];
-	int n = sizeof(coins) / sizeof(coins[0]);
-	int sum = 5;
-     cout<<"Enter the no of array element";
-    cin>>n;
-    for(i=1;i<n;i++)
-    {
+    // Base case: There is one way to make sum = 0 (empty set)
+    dp[0] = 1;
 
-        cin>>coins[i];
+    // Iterate through each coin and update the dp array
+    for (int i = 0; i < n; ++i) {
+        for (int j = coins[i]; j <= sum; ++j) {
+            dp[j] += dp[j - coins[i]];
+        }
     }
 
-	cout << " " << count(coins, n, sum);
+    // The result is stored at the last index of dp array
+    return dp[sum];
+}
 
-	return 0;
+int main() {
+    int sum1 = 4;
+    int coins1[] = {1, 2, 3};
+    int output1 = countCoinChangeWays(coins1, sizeof(coins1) / sizeof(coins1[0]), sum1);
+    cout << "Input: sum = " << sum1 << ", coins = {1, 2, 3}, Output: " << output1 << endl;
+
+    int sum2 = 10;
+    int coins2[] = {2, 5, 3, 6};
+    int output2 = countCoinChangeWays(coins2, sizeof(coins2) / sizeof(coins2[0]), sum2);
+    cout << "Input: sum = " << sum2 << ", coins = {2, 5, 3, 6}, Output: " << output2 << endl;
+
+    return 0;
 }
